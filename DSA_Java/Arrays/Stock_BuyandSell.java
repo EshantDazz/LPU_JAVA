b175d61c064edeae132e3f52b7ca146aa8ebc173
Eshant Das <95016576+EshantDazz@users.noreply.github.com>
2023-01-20 21:22:04 +0530

    Add files via upload

---
+++ DSA_Java/Arrays/Stock_BuyandSell.java
@@ -0,0 +1,16 @@
+public class Stock_BuyandSell {
+    static public void main(String args[])
+    {
+         int arr[]={1,5,3,1,2,8};
+         int n=arr.length;
+         int profit=0;
+         for(int i=1;i<n;i++)
+         {
+            if(arr[i]>arr[i-1])
+            profit+=arr[i]-arr[i-1];
+         }
+         System.out.println(profit);
+
+    }
+    
+}
